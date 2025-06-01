113d059d3f4b69aaa39873307b9703edc10efbb5
narzullayevj99 <javohirnarzullayeb@gmail.com>
2025-06-01 16:14:41 +0500

    first commit

---
+++ src/main/java/uz/narzullayev/javohir/UserLar.java
@@ -0,0 +1,13 @@
+package uz.narzullayev.javohir;
+
+public class UserLar {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+    public String password() {
+        return username;
+    }
+}
